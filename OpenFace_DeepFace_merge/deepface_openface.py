fe0370629280e614dedb1db25907ab006539781a
DESKTOP-57NTLJA\hwojc <h.wojcik@seznam.cz>
2023-04-14 13:43:56 +0200

    Detection of hands in face area

--- OpenFace_DeepFace_merge/deepface_openface.py
+++ OpenFace_DeepFace_merge/deepface_openface.py
@@ -6,22 +6,44 @@ import os
 import cv2  
 from deepface import DeepFace
 
+def deleteFolderContents(folder_path):
+    """
+    Deletes all files and subdirectories inside a folder, but not the folder itself.
+    """
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)  # remove file
+            elif os.path.isdir(file_path):
+                deleteFolderContents(file_path)  # recurse and remove subdirectories
+                os.rmdir(file_path)  # remove directory
+        except Exception as e:
+            print(f"Failed to delete {file_path}. Reason: {e}")
+
+
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     raise IOError("Cannot open webcam")
 
 # Path to FaceLandmarkImg.exe and image file
 #exePath  = r"C:\Users\hwojc\Desktop\Diplomka\Open Face\OpenFace_2.2.0_win_x64\FeatureExtraction.exe"
-exePath  = r"C:\Users\hwojc\Desktop\Diplomka\Repo\OpenFace-OpenFace_2.2.0\x64\Release\FeatureExtraction.exe"
+exePath  = r"C:\Users\hwojc\Desktop\Diplomka\Open Face\OpenFace_2.2.0_win_x64\FeatureExtraction.exe"
 
-imageDirPath  = r"C:\Users\hwojc\Desktop\Diplomka\Repo\OpenFace_DeepFace_merge\images"
+imageDirPathBase  = r"C:\Users\hwojc\Desktop\Diplomka\Repo\OpenFace_DeepFace_merge\images"
 imgID = 0
 
 # Output directory
 outDir = r"C:\Users\hwojc\Desktop\Diplomka\Repo\OpenFace_DeepFace_merge\processed"
 
+#Delete previous data
+deleteFolderContents(imageDirPathBase)
+deleteFolderContents(outDir)
+
 outputFilePath = outDir + r"\images.csv"
 lastPosition = 0
+checkCSV = False
+gCheckProc = False
 
 #save first image
 """imgName = str(imgID) + '.jpg'
@@ -30,6 +52,12 @@ cv2.imwrite(os.path.join(imageDirPath, imgName), frame)"""
 
 start = time.time()
 
+imgDirIndex = 1
+imgSavedCount = 0
+imageDirPath =  os.path.join(imageDirPathBase, str(imgDirIndex))
+if not os.path.exists(imageDirPath):
+    os.makedirs(imageDirPath)
+
 
 while True:
 
@@ -37,23 +65,35 @@ while True:
     if frameRecieved:
         try:
             result = DeepFace.analyze(frame, actions = ['emotion'], enforce_detection= True)
-            cv2.putText(frame, result['dominant_emotion'],(50,50), cv2.FONT_ITALIC, 1, (0,0,0), 2, cv2.LINE_4)
+            frame = cv2.putText(frame, result['dominant_emotion'],(50,50), cv2.FONT_ITALIC, 1, (0,0,0), 2, cv2.LINE_4)
+            frame = cv2.putText(frame, str(imgID), (50,100), cv2.FONT_ITALIC, 1, (0,0,0), 2)
+
             imgName = str(imgID) + '.jpg'
             imgPath = os.path.join(imageDirPath, imgName)
             cv2.imwrite(imgPath, frame)
-            
-            process = subprocess.run([exePath, "-f", imgPath , "-aus","-out_dir", outDir])
-            csvName = str(imgID) + '.csv'
-            outputFilePath = os.path.join(outDir, csvName)
-
+            imgSavedCount = imgSavedCount + 1
             imgID = imgID + 1
 
+            if(imgSavedCount % 30 == 0):
+                #process = subprocess.run([exePath, "-fdir", imageDirPath , "-aus","-out_dir", outDir])
+                process = subprocess.Popen([exePath, "-fdir", imageDirPath , "-aus","-out_dir", outDir])
+                csvName = str(imgDirIndex) + '.csv'
+                outputFilePath = os.path.join(outDir, csvName)
+                imgDirIndex = imgDirIndex + 1
+                imgSavedCount = 0
+                imageDirPath = os.path.join(imageDirPathBase, str(imgDirIndex))
+                if not os.path.exists(imageDirPath):
+                    os.makedirs(imageDirPath)
+                gCheckProc = True
+            cv2.imshow('Video', frame)
         except:
-            cv2.putText(frame,'Cannot detect face',(50,50), cv2.FONT_ITALIC, 1, (0,0,0), 2, cv2.LINE_4)
-        cv2.imshow('Video', frame)
+            frame = cv2.putText(frame,'Cannot detect face',(50,50), cv2.FONT_ITALIC, 1, (0,0,0), 2, cv2.LINE_4)
+            cv2.imshow('Video', frame)
+
     else:
         print("Frame not recieved")
-
+        
+    """
     try:
         with open(outputFilePath, 'r') as csvFile:         
             # Create a new CSV reader object
@@ -63,12 +103,13 @@ while True:
                 # Do something with the row data
                 print(row)         
         # Wait for a short time before checking the file again
-        time.sleep(0.1)
+        #time.sleep(0.1)
         
     except FileNotFoundError:
         # Handle the case where the file doesn't exist yet
-        time.sleep(0.1)
-    
+        #time.sleep(0.1)
+        print("CSV doesnt exist yet")
+    """
     #Get key, if ESC, then end loop
     c = cv2.waitKey(1)
     if c == 27:
