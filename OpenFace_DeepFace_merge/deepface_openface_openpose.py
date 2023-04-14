f5681da1d7791f241a2907d55127e9b8025eb992
DESKTOP-57NTLJA\hwojc <h.wojcik@seznam.cz>
2023-04-14 14:51:30 +0200

    cleaning the code

--- OpenFace_DeepFace_merge/deepface_openface_openpose.py
+++ OpenFace_DeepFace_merge/deepface_openface_openpose.py
@@ -34,6 +34,79 @@ def angleFromVertical(p1, p2):
     angle = math.atan2(deltaX, deltaY)
     return -math.degrees(angle)
 
+def opDrawSkeleton(frame, points, POSE_PAIRS):
+    # Draw Skeleton
+    for pair in POSE_PAIRS:
+        partA = pair[0]
+        partB = pair[1]
+
+        if points[partA] and points[partB]:
+            cv2.line(frame, points[partA], points[partB], (0, 255, 255), 2)
+            cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+            
+        if points[0] and points[1]:
+            diameter = math.dist(points[0], points[1])
+            midPoint = [0, 0]
+            # Define two points
+            pointArr0 = np.array(points[0])
+            pointArr1 = np.array(points[1])
+
+            # Calculate the midpoint
+            midpoint = (pointArr0 + pointArr1) / 2
+            center = tuple(np.round(midpoint).astype(int))
+            ellAngle = angleFromVertical(points[0], points[1])
+            #cv2.circle(frame, center, round((diameter/2) * 1.2), (255, 0, 0), thickness = 2)
+            center2 = tuple(np.round(points[1]).astype(int))
+            cv2.ellipse(frame, center, (int(diameter/1.5), int(diameter/1.2)), ellAngle, 0, 360, (255, 0, 0), thickness = 2)
+
+            e = Ellipse((int(center[0]), int(center[1])), 2*int(diameter/1.5), 2*int(diameter/1.2), ellAngle)
+            rightWrist = points[4]
+            leftWrist = points[7]
+            if(rightWrist and not leftWrist):
+                if e.contains_point(rightWrist):
+                    print("Right hand in face area")
+            elif(leftWrist and not rightWrist):
+                if e.contains_point(leftWrist):
+                    print("Left hand in face area")
+            elif(rightWrist and leftWrist):
+                if e.contains_point(leftWrist) and e.contains_point(rightWrist):
+                    print("Both hands in face area")
+            else:
+                print("No hand in face area")
+    return frame
+
+def opGetPoints(output, frame, frameCopy):
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+    threshold = 0.1
+     
+    H = output.shape[2]
+    W = output.shape[3]
+
+    # Empty list to store the detected keypoints
+    points = []
+
+    for i in range(nPoints):
+        # confidence map of corresponding body's part.
+        probMap = output[0, i, :, :]
+
+        # Find global maxima of the probMap.
+        minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+        
+        # Scale the point to fit on the original image
+        x = (frameWidth * point[0]) / W
+        y = (frameHeight * point[1]) / H
+
+        if prob > threshold : 
+            cv2.circle(frameCopy, (int(x), int(y)), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
+            cv2.putText(frameCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
+
+            # Add the point to the list if the probability is greater than the threshold
+            points.append((int(x), int(y)))
+        else :
+            points.append(None)
+    return points
+
 MODE = "MPI"
 
 if MODE == "COCO":
@@ -78,7 +151,6 @@ outputFilePath = outDir + r"\images.csv"
 checkCSV = False
 gCheckProc = False
 
-
 start = time.time()
 
 imgDirIndex = 1
@@ -121,19 +193,9 @@ while True:
             #cv2.imshow('Video', frame)
     
         frameCopy = np.copy(frame)
-        frameWidth = frame.shape[1]
-        frameHeight = frame.shape[0]
-        threshold = 0.1
+        net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+        net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
-        
-
-        if args.device == "cpu":
-            net.setPreferableBackend(cv2.dnn.DNN_TARGET_CPU)
-            print("Using CPU device")
-        elif args.device == "gpu":
-            net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-            net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-            #print("Using GPU device")
 
         t = time.time()
         # input image dimensions for the network
@@ -143,76 +205,10 @@ while True:
                                 (0, 0, 0), swapRB=False, crop=False)
 
         net.setInput(inpBlob)
-
         output = net.forward()
-        #print("time taken by network : {:.3f}".format(time.time() - t))
-
-        H = output.shape[2]
-        W = output.shape[3]
-
-        # Empty list to store the detected keypoints
-        points = []
-
-        for i in range(nPoints):
-            # confidence map of corresponding body's part.
-            probMap = output[0, i, :, :]
-
-            # Find global maxima of the probMap.
-            minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
-            
-            # Scale the point to fit on the original image
-            x = (frameWidth * point[0]) / W
-            y = (frameHeight * point[1]) / H
-
-            if prob > threshold : 
-                cv2.circle(frameCopy, (int(x), int(y)), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
-                cv2.putText(frameCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
-
-                # Add the point to the list if the probability is greater than the threshold
-                points.append((int(x), int(y)))
-            else :
-                points.append(None)
-
-        # Draw Skeleton
-        for pair in POSE_PAIRS:
-            partA = pair[0]
-            partB = pair[1]
-
-            if points[partA] and points[partB]:
-                cv2.line(frame, points[partA], points[partB], (0, 255, 255), 2)
-                cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
-                
-            if points[0] and points[1]:
-                diameter = math.dist(points[0], points[1])
-                midPoint = [0, 0]
-                # Define two points
-                pointArr0 = np.array(points[0])
-                pointArr1 = np.array(points[1])
-
-                # Calculate the midpoint
-                midpoint = (pointArr0 + pointArr1) / 2
-                center = tuple(np.round(midpoint).astype(int))
-                ellAngle = angleFromVertical(points[0], points[1])
-                #cv2.circle(frame, center, round((diameter/2) * 1.2), (255, 0, 0), thickness = 2)
-                center2 = tuple(np.round(points[1]).astype(int))
-                cv2.ellipse(frame, center, (int(diameter/1.5), int(diameter/1.2)), ellAngle, 0, 360, (255, 0, 0), thickness = 2)
-
-                e = Ellipse((int(center[0]), int(center[1])), 2*int(diameter/1.5), 2*int(diameter/1.2), ellAngle)
-                rightWrist = points[4]
-                leftWrist = points[7]
-                if(rightWrist and not leftWrist):
-                    if e.contains_point(rightWrist):
-                        print("Right hand in face area")
-                elif(leftWrist and not rightWrist):
-                    if e.contains_point(leftWrist):
-                        print("Left hand in face area")
-                elif(rightWrist and leftWrist):
-                    if e.contains_point(leftWrist) and e.contains_point(rightWrist):
-                        print("Both hands in face area")
-                else:
-                    print("No hand in face area")
-
-        #cv2.imshow('Output-Keypoints', frameCopy)
+        points = opGetPoints(output, frame, frameCopy)
+        frame = opDrawSkeleton(frame, points, POSE_PAIRS)
+        
         cv2.imshow('Output-Skeleton', frame)
 
     else:
