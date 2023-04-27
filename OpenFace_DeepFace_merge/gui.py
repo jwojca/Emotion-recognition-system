d97b488c2c01c476d12ae83525e3fc6a5b75a5fa
DESKTOP-57NTLJA\hwojc <h.wojcik@seznam.cz>
2023-04-27 18:58:32 +0200

    analyze and train menu added

--- OpenFace_DeepFace_merge/gui.py
+++ OpenFace_DeepFace_merge/gui.py
@@ -121,17 +121,43 @@ def butt6Cmd():
         button6["text"] = "Turn on"
     print(f"Button 6 state: {button6State}")
 
-def butt7Cmd(rectCanvas, rectPos):
+def butt7Cmd(analyzeCanvas, trainCanvas, rectPos):
     global button7State
     button7State = not button7State
+    window1 = findWindow("tracking result")
+    position1 = (800, 35)
+    height1 = 420
+    width1 = 640
+    
+    window2 = findWindow("action units")
+    position2 = (800, 455)
+    width2 = 585
+    height2 = 370
+
     if button7State:
-        button7["text"] = "Turn off"
-        rectCanvas.place_forget()
+        button7["text"] = "To analyze"
+        analyzeCanvas.place_forget()
+        trainCanvas.place(x = rectPos[0], y = rectPos[1])
+        showWindow(window1, position1, width1, height1)
+        showWindow(window2, position2, width2, height2)
+       
     else:
-        rectCanvas.config(state='normal')
-        button7["text"] = "Turn on"
-        rectCanvas.place(x = rectPos[0], y = rectPos[1])
+        analyzeCanvas.config(state='normal')
+        button7["text"] = "To train"
+        analyzeCanvas.place(x = rectPos[0], y = rectPos[1])
+        trainCanvas.place_forget()
+        hideWindow(window1)
+        hideWindow(window2)
     print(f"Button 7 state: {button7State}")
+
+def butt8Cmd():
+    global button8State
+    button8State = not button8State
+    if button8State:
+        button8["text"] = "Stop"
+    else:
+        button8["text"] = "Start"
+    print(f"Button 8 state: {button8State}")
 
 def findWindow(windowName):
     window = win32gui.FindWindow(None, windowName)
@@ -142,75 +168,103 @@ def findWindow(windowName):
         print("Window not found")
     return window
 
+def showWindow(window, position, width, height):
+    win32gui.ShowWindow(window, win32con.SW_SHOWNORMAL)
+    style = win32gui.GetWindowLong(window, win32con.GWL_STYLE)
+    style &= ~win32con.WS_CAPTION
+    win32gui.SetWindowLong(window, win32con.GWL_STYLE, style)
+    win32gui.MoveWindow(window, position[0], position[1], width, height, True)
+    win32gui.SetWindowPos(window, win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+
+def hideWindow(window):
+    win32gui.ShowWindow(window, win32con.SW_MINIMIZE)
+
 def tkInit():
-    global root, tableCanvas, webcamCanvas, rectCanvas
-    global button1, button2, button3, button4, button5, button6, button7
+    global root, tableCanvas, webcamCanvas, analyzeCanvas
+    global button1, button2, button3, button4, button5, button6, button7, button8
+
+    buttPosOrig = (130, 7)
+    buttYOffset = 40
+    buttTextPosOrig = (5, 10)
+    webcamPos = (90, 10)
 
     root = tk.Tk()
     root.geometry("1920x1080")
     root.state("zoomed")
-    root.title('Emotion Recognition System')
-    webcamPos = (90, 10)
-
-    buttPosOrig = (180, 10)
-    buttYOffset = 40
-
-    buttTextPosOrig = (50, 10)
-
+    root.title('Emotion Recognition System')  
+    
 
     webcamCanvas = tk.Canvas(root, width=gWebcamCanvasShape[0], height=gWebcamCanvasShape[1])
     webcamCanvas.place(x = webcamPos[0], y = webcamPos[1])
 
-    rectWidth = 250
+    rectWidth = 200
     rectHeight = 350
-    rectPos = (40, 450)
+    rectPos = (85, 510)
     rectBorder = 0
-    rectCanvas = tk.Canvas(root, width = rectWidth + 1, height = rectHeight + 1)
-    rectCanvas.create_rectangle(rectBorder + 1, rectBorder + 1, rectWidth, rectHeight, width = rectBorder)
-    rectCanvas.create_text(buttTextPosOrig[0], buttTextPosOrig[1], text = "Start button", fill="black", anchor=tk.NW)
-    rectCanvas.create_text(buttTextPosOrig[0], buttTextPosOrig[1] + buttYOffset, text = "Table", fill="black",anchor=tk.NW)
-    rectCanvas.create_text(buttTextPosOrig[0], buttTextPosOrig[1] + 2 * buttYOffset, text = "OpenFace webcam", fill="black", anchor=tk.NW)
-    rectCanvas.create_text(buttTextPosOrig[0], buttTextPosOrig[1] + 3 * buttYOffset, text = "Action units", fill="black", anchor=tk.NW)
-    rectCanvas.create_text(buttTextPosOrig[0], buttTextPosOrig[1] + 4 * buttYOffset, text = "OpenPose skeleton", fill="black", anchor=tk.NW)
-    rectCanvas.create_text(buttTextPosOrig[0], buttTextPosOrig[1] + 5 * buttYOffset, text = "Face areas", fill="black", anchor=tk.NW)
-    rectCanvas.place(x = rectPos[0], y = rectPos[1])
+    analyzeCanvas = tk.Canvas(root, width = rectWidth + 1, height = rectHeight + 1)
+    analyzeCanvas.create_rectangle(rectBorder + 1, rectBorder + 1, rectWidth, rectHeight, width = rectBorder)
+    analyzeCanvas.create_text(buttTextPosOrig[0], buttTextPosOrig[1], text = "Start button", fill="black", anchor=tk.NW)
+    analyzeCanvas.create_text(buttTextPosOrig[0], buttTextPosOrig[1] + buttYOffset, text = "Table", fill="black",anchor=tk.NW)
+    analyzeCanvas.create_text(buttTextPosOrig[0], buttTextPosOrig[1] + 2 * buttYOffset, text = "OpenFace webcam", fill="black", anchor=tk.NW)
+    analyzeCanvas.create_text(buttTextPosOrig[0], buttTextPosOrig[1] + 3 * buttYOffset, text = "Action units", fill="black", anchor=tk.NW)
+    analyzeCanvas.create_text(buttTextPosOrig[0], buttTextPosOrig[1] + 4 * buttYOffset, text = "OpenPose skeleton", fill="black", anchor=tk.NW)
+    analyzeCanvas.create_text(buttTextPosOrig[0], buttTextPosOrig[1] + 5 * buttYOffset, text = "Face areas", fill="black", anchor=tk.NW)
+    analyzeCanvas.place(x = rectPos[0], y = rectPos[1])
+
+    trainCanvas = tk.Canvas(root, width = rectWidth + 1, height = rectHeight + 1)
+    trainCanvas.create_text(buttTextPosOrig[0], buttTextPosOrig[1], text = "Select emotion", fill="black", anchor=tk.NW)
+    trainCanvas.create_text(buttTextPosOrig[0], buttTextPosOrig[1] + buttYOffset, text = "Start learning", fill="black",anchor=tk.NW)
+
+    headerHeight = 60
+    headerPos = (rectPos[0], 440)
+    headerCanvas = tk.Canvas(root, width=rectWidth, height= headerHeight)
+    headerCanvas.place(x = headerPos[0], y = headerPos[1])
+    headerCanvas.create_text(buttTextPosOrig[0], buttTextPosOrig[1], text = "Change mode", font=('freemono', 10,'bold'), fill="black", anchor=tk.NW)
 
     tableCanvas = tk.Canvas(root, width=gTableCanvasShape[0], height=gTableCanvasShape[1])
     tableCanvas.place(x = 316, y = 450)
+    
 
 
     # Create the buttons
     button1Pos = (buttPosOrig[0], buttPosOrig[1])
-    button1 = tk.Button(rectCanvas, text= "Start" , command = butt1Cmd)
+    button1 = tk.Button(analyzeCanvas, text= "Start" , command = butt1Cmd)
     button1.place(x = button1Pos[0], y = button1Pos[1])
 
     button2Pos = (buttPosOrig[0], buttPosOrig[1] + buttYOffset)
-    button2 = tk.Button(rectCanvas, text="Turn on", command=butt2Cmd)
+    button2 = tk.Button(analyzeCanvas, text="Turn on", command=butt2Cmd)
     button2.place(x = button2Pos[0], y = button2Pos[1])
 
     button3Pos = (buttPosOrig[0], buttPosOrig[1] + 2*buttYOffset)
-    button3 = tk.Button(rectCanvas, text="Turn on", command=butt3Cmd)
+    button3 = tk.Button(analyzeCanvas, text="Turn on", command=butt3Cmd)
     button3.place(x = button3Pos[0], y = button3Pos[1])
 
     button4Pos = (buttPosOrig[0], buttPosOrig[1] + 3*buttYOffset)
-    button4 = tk.Button(rectCanvas, text="Turn on", command=butt4Cmd)
+    button4 = tk.Button(analyzeCanvas, text="Turn on", command=butt4Cmd)
     button4.place(x = button4Pos[0], y = button4Pos[1])
 
     button5Pos = (buttPosOrig[0], buttPosOrig[1] + 4*buttYOffset)
-    button5 = tk.Button(rectCanvas, text="Turn on", command=butt5Cmd)
+    button5 = tk.Button(analyzeCanvas, text="Turn on", command=butt5Cmd)
     button5.place(x = button5Pos[0], y = button5Pos[1])
 
     button6Pos = (buttPosOrig[0], buttPosOrig[1] + 5*buttYOffset)
-    button6 = tk.Button(rectCanvas, text="Turn on", command=butt6Cmd)
+    button6 = tk.Button(analyzeCanvas, text="Turn on", command=butt6Cmd)
     button6.place(x = button6Pos[0], y = button6Pos[1])
 
-    button7Pos = (buttPosOrig[0], buttPosOrig[1] + 6*buttYOffset)
-    button7 = ttk.Button(root, text="Turn on", command= lambda: butt7Cmd(rectCanvas, rectPos))
+
+    #Header canvas
+    button7Pos = (buttPosOrig[0], buttPosOrig[1])
+    button7 = tk.Button(headerCanvas, text="To train", command= lambda: butt7Cmd(analyzeCanvas, trainCanvas, rectPos))
     button7.place(x = button7Pos[0], y = button7Pos[1])
 
-    emOption = StringVar(root)
+    #Train
+    emOption = StringVar(trainCanvas)
     emOption.set(OPTIONS[0])
-    dropDown = OptionMenu(root, emOption, *OPTIONS)
-    dropDown.pack()
+    dropDown = OptionMenu(trainCanvas, emOption, *OPTIONS)
+    dropDown.place(x = buttPosOrig[0], y = buttPosOrig[1] - 3)
+
+    button8Pos = (buttPosOrig[0], buttPosOrig[1] + buttYOffset)
+    button8 = tk.Button(trainCanvas, text="Start", command=butt8Cmd)
+    button8.place(x = button8Pos[0], y = button8Pos[1])
 
     return root
